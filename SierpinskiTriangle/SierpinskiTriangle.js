bad4be95358be044ed6908c5929ab8ca1e1d9710
Nicholas Evans <nicholas.evans97@outlook.com>
2022-04-24 12:44:51 -0300

    Updated logic and renamed variables

--- SierpinskiTriangle/SierpinskiTriangle.js
+++ SierpinskiTriangle/SierpinskiTriangle.js
@@ -3,7 +3,7 @@ let cornerPositions;
 let curX, curY;
 let prevX, prevY;
 
-let curCornerCount;
+let cornerCount;
 let newCornerCount;
 let minCornerCount;
 let maxCornerCount;
@@ -11,9 +11,9 @@ let maxCornerCount;
 let pointSize;
 let cornerSize
 
-let iterations, maxIterations;
-
-let diceCount;
+let iteration
+let maxIterations;
+let iterationsPerCycle
 
 let multiplier, divider;
 
@@ -26,37 +26,47 @@ function setup() {
 
 function draw() {
 
-    if(iterations <= maxIterations) {
-        drawNewPoint();
-        updateDisplayData();
+
+    if(iteration <= maxIterations)
+    {
+        for(let _=0; _<iterationsPerCycle; _++)
+        {
+            if(iteration <= maxIterations)
+            {
+                drawNewPoint();
+                updateDisplayData();
+                iteration++;
+            }
+        }
+        
     }
 }
 
 function setupData() {
     background(0);
-    stroke(0,255,0);
-    fill(0,255,0);
     
-    curCornerCount = 3;
-    multiplier = curCornerCount-2;
-    divider = curCornerCount-1;
-
+    cornerCount = 3;
+    multiplier = cornerCount-2;
+    divider = cornerCount-1;
+    
     newCornerCount = 5;
     minCornerCount = 3;
     maxCornerCount = 8;
-
-    cornerPositions = createNgon(300,300,85,curCornerCount);
-
-    diceCount = new Array(curCornerCount);
-    for(let i=0; i<diceCount.length; i++) {
-        diceCount[i] = 0;
-    }
-
+    
     pointSize = 0.1;
-    cornerSize = 5;
-
-    iterations = 0;
+    cornerSize = 3;
+    
+    iteration = 1;
     maxIterations = 1000000;
+    iterationsPerCycle = 37;
+    
+    stroke(0,255,0);
+    fill(0,255,0);
+    
+    cornerPositions = createNgon(300,300,85,cornerCount);
+
+    fill(255,0,0);
+    stroke(255,0,0);
 
     curX = (cornerPositions[0].x + cornerPositions[1].x * multiplier)/divider;
     curY = (cornerPositions[0].y + cornerPositions[1].y * multiplier)/divider;
@@ -66,19 +76,20 @@ function createNgon(x, y, radius, nPoints) {
 
     let ngonPoints = new Array(nPoints);
 
-    let angle = TWO_PI / curCornerCount;
+    let angle = TWO_PI / cornerCount;
     let index = 0;
     for(let a=0; a<TWO_PI; a += angle) {
-        if(index >= curCornerCount) {
+        if(index >= cornerCount) {
             break;
         }
     
-        let rotationOffset = PI/(curCornerCount * 2);
+        let rotationOffset = PI/(cornerCount * 2);
         let pointX = (x + cos(a+rotationOffset) * radius * nPoints);
         let pointY = (y + sin(a+rotationOffset) * radius * nPoints);
 
         ngonPoints[index] = createVector(pointX, pointY);
 
+        stroke(0,255,0);
         circle(pointX, pointY, cornerSize);
 
         index++;
@@ -88,25 +99,19 @@ function createNgon(x, y, radius, nPoints) {
 }
 
 function drawNewPoint() {
-    let rand = Math.floor(random(curCornerCount));
-    diceCount[rand] += 1;
+    let rand = Math.floor(random(cornerCount));
 
     prevX = curX;
     prevY = curY;
     circle(prevX, prevY, pointSize);
 
-    fill(255,0,0);
-    stroke(255,0,0);
-
     curX = (curX + cornerPositions[rand].x * multiplier)/divider;
     curY = (curY + cornerPositions[rand].y * multiplier)/divider;
 
     circle(curX, curY, pointSize);
-
-    iterations++;
 }
 
 function updateDisplayData() {
-    document.getElementById("iterations").innerHTML = "Iterations: "+iterations;
+    document.getElementById("iterations").innerHTML = "Iterations: "+iteration;
 }
 
